d79bf64411a9a7d5f71cc963d419841499668bda
Caro <pastorecarolinae@gmail.com>
2020-09-07 19:44:25 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/imsocaroline");
+    const json = await data.json();
+
+    this.setState({
+      data: json,
+    });
+  }
+
+  // handleClick() {
+  //   this.setState({
+  //     data: { login: "yo" },
+  //   });
+  // }
+
+  render() {
+    console.log("RENDER");
+    return (
+      <>
+        <Component1 />
+        <Component2 />
+        <p>{this.state.data.login}</p>
+        {/* <button onClick={() => this.handleClick()}>Resetear</button> */}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Component1/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Component1 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+    };
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({
+      text: value,
+    });
+  }
+
+  render() {
+    const { text } = this.state;
+    return (
+      <>
+        <input type="text" onChange={(e) => this.handleChange(e)} />
+        <p>{text}</p>
+      </>
+    );
+  }
+}
+
+export default Component1;
